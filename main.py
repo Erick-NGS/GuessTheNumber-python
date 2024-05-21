04212ca925355209c95c12a616c99d1c94a2d54e
Erick Silva <engs.3003@gmail.com>
2024-05-21 13:29:20 -0300

    adding the answer to main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import art, random, utils
 
+num_answer = random.randint(1, 100)
+
 print(art.welcome)
 print("Welcome to the 'Guessing Number Game'!")
 print("The goal is to guess a random number between 1 & 100. Good luck and may the force be with you!")
+# Answer
+# print(f"The answer is {num_answer}")
