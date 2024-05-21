eee0fd028171e0529c19ec7f5b958ca313476d6b
Erick Silva <engs.3003@gmail.com>
2024-05-20 23:50:43 -0300

    add imports, add welcome text

---
+++ main.py
@@ -0,0 +1,5 @@
+import art, random, utils
+
+print(art.welcome)
+print("Welcome to the 'Guessing Number Game'!")
+print("The goal is to guess a random number between 1 & 100. Good luck and may the force be with you!")
